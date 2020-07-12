5e16784481c72113f79978560ab45aae5bdcb5b0
jugraj280 <57304207+jugraj280@users.noreply.github.com>
2020-07-12 22:16:31 +0530

    assignment 1

---
+++ assignment 1/script.js
@@ -0,0 +1,112 @@
+//exercise 1
+//var x =10;
+//var y =20;
+//var sum =x +y;
+//alert("sum of two numbers:"+sum )
+
+//exercise 2
+//var x =30;
+//var y =20;
+//var substraction =x -y;
+//alert("substraction of two numbers:"+substraction)
+ 
+ //exercise 3
+//var x =20;
+//var y =20;
+//var multiplication =x *y;
+//alert("multiplication of two numbers:"+ multiplication)
+ 
+ //homework 1
+//var x =20;
+//var y =2;
+//var division =x /y;
+//alert("division of two numbers:"+ division)
+ 
+ //exercise 4
+//var x = Number(prompt("enter your number one:"));
+//var y = Number(prompt("enter your number two:"));
+//var division =x /y;
+//alert("division of two numbers:"+ division)
+
+//homework 2 
+//var a = Number(prompt("enter your number one:"));
+//var b = Number(prompt("enter your number two:"));
+//var c = Number(prompt("enter your number three:"));
+//var d = Number(prompt("enter your number four:"));
+//var e = Number(prompt("enter your number five:"));
+//var f = Number(prompt("enter your number six:"));
+//var sum =a+b+c+d+e+f;
+//alert("sum of six numbers:"+ sum)
+
+//exercise 5
+//var name = prompt("enter your name:");
+//var roll_no= Number(prompt("enter your roll_no:"));
+//var mobile_no = Number(prompt("enter your mobile_no:"));
+//alert(name+roll_no+mobile_no)
+
+//homework 3
+//var x = 2;
+//var PIE= 3.14;
+//var radius = Number(prompt("enter your radius:"));
+//var multiplication =x * PIE * radius;
+//alert( "your circumference is :" + multiplication);
+
+//homework 4
+//var PIE= 3.14;
+//var radius = Number(prompt("enter your radius:"));
+//var multiplication = PIE * radius *radius;
+//alert( "your surface area is :" + multiplication);
+
+//exercise 6
+//var a = Number(prompt( "enter number a :"));
+//var b = Number(prompt( "enter number b :"));
+ //if(a>b){
+ //	alert("a is largest, b is smallest.");
+ //}
+//if (a<b) {
+//	alert("b is largest, a is smallest.");
+//}
+
+//homework 5
+var a = Number(prompt( "enter number a :"));
+var b = Number(prompt( "enter number b :"));
+var c = Number(prompt( "enter number c :"));
+ if(a>b && a>c){
+ 	alert("a is largest than b and c");
+ }
+if(b>a && b>c){
+ 	alert("b is largest than a and c");
+ }
+ if(c>a && c>b){
+ 	alert("c is largest than a and b");
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
