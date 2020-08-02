a2d0eaf4e4582f233cf043c901ee29a934887c77
jugraj280 <57304207+jugraj280@users.noreply.github.com>
2020-08-02 21:14:59 +0530

    mid term

---
+++ mid term s.l/script.js
@@ -0,0 +1,25 @@
+//ans 1
+//var x = 4/3;
+//var PIE= 3.14;
+//var radius = Number(prompt("enter your radius:"));
+//var multiplication =x * PIE * radius * radius;
+//alert( "your volume is :" + multiplication);
+
+//ans 2
+//var age = Number(prompt("enter your age:"));
+//if( age >=1 && <= 19){
+//	alert("You are not a major yet.");
+//}
+//if( age >=20 && <= 45){
+//	alert("You are major,but not senior yet.");
+//}
+//if( age >=46 && <= 90){
+//	alert("You are senior,but not yet retired.");
+//}
+
+//ans 3
+var i = 1;
+while(i <= 25) {
+	alert("Bonjour");
+	i = i + 1 ;
+}
