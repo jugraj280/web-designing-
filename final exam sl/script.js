227406f50f930032f54fba1d66aaaf9358c15db6
jugraj280 <57304207+jugraj280@users.noreply.github.com>
2020-08-17 04:13:22 +0530

    final exam

---
+++ final exam sl/script.js
@@ -0,0 +1,6 @@
+function getdata(){
+	var firstname = document.getElementById("fname").value;
+	var secondname = document.getElementById("sname").value;
+	var thridname = document.getElementById("tname").value;
+	document.getElementById("displayData").innerHTML="Firstname:" + firstName: +"<br> second name:" + second name +"<br> thrid name:" + thrid name;
+}
