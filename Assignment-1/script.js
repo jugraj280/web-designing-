1bc4f4cd59c95db9bcd75656720269f19d5f421e
jugraj280 <57304207+jugraj280@users.noreply.github.com>
2020-11-01 01:49:09 +0530

    assignment-1

---
+++ Assignment-1/script.js
@@ -0,0 +1,11 @@
+function hideHeading() {
+    $("h1").hide(); 
+    $("#hideHeading").hide(); 
+    $("#showHeading").show(); 
+}
+
+function showHeading() {
+    $("h1").show();  
+    $("#hideHeading").show();
+    $("#showHeading").hide();
+}
